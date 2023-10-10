3997a1fc7a79cb88fe92482467a6da30c2cbed7e
TherathVineeth <deerakrish987@gmail.com>
2023-10-10 20:30:32 +0530

    Crud-Completed

--- Moble_List_Application/Moble_List_Application/Program.cs
+++ Moble_List_Application/Moble_List_Application/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Mobiles}/{action=Index}/{id?}");
 
 app.Run();
